2db4a6fd58074af630012d429d74f425589a0345
David Meyer <thedmeyer@gmail.com>
2019-03-30 19:25:45 -0600

    :white_check_mark: adding more tests

--- test/test.js
+++ test/test.js
@@ -15,4 +15,60 @@ describe('parseXML()', function () {
 		expect(output).to.be.equal(result);
 
 	});
+
+	it('should parse multiple tags', function () {
+
+		// 1. ARRANGE
+		var input = '<a>blah</a><b>wow</b>';
+		var result = "{'a':'blah'},{'b':'wow'}";
+
+		// 2. ACT
+		var output = parseXML(input);
+
+		// 3. ASSERT
+		expect(output).to.be.equal(result);
+
+	});
+
+	it('should parse nested tags', function () {
+
+		// 1. ARRANGE
+		var input = '<a><b>wow</b></a>';
+		var result = "{'a':{'b':'wow'}}";
+
+		// 2. ACT
+		var output = parseXML(input);
+
+		// 3. ASSERT
+		expect(output).to.be.equal(result);
+
+	});
+
+	it('should parse multi-char tags', function () {
+
+		// 1. ARRANGE
+		var input = '<aaaa><b>wow</b></aaaa>';
+		var result = "{'aaaa':{'b':'wow'}}";
+
+		// 2. ACT
+		var output = parseXML(input);
+
+		// 3. ASSERT
+		expect(output).to.be.equal(result);
+
+	});
+
+	it('should parse multi-nested tags', function () {
+
+		// 1. ARRANGE
+		var input = '<aaaa><b>wow</b><b>wow</b></aaaa>';
+		var result = "{'aaaa':{'b':'wow'},{'b':'wow'}}";
+
+		// 2. ACT
+		var output = parseXML(input);
+
+		// 3. ASSERT
+		expect(output).to.be.equal(result);
+
+	});
 });
